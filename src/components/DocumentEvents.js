1c6b8d82783de5bb2c172e9a143d1989f4f0367e
patrickmarabeas <patrick@marabeas.io>
2016-04-28 13:58:31 +1000

    Share observables

--- src/components/DocumentEvents.js
+++ src/components/DocumentEvents.js
@@ -1,7 +1,7 @@
 import { Observable } from 'rx';
 
-const click$      = Observable.fromEvent(document, 'click');
-const scroll$     = Observable.fromEvent(document, 'mousewheel');
+const click$      = Observable.fromEvent(document, 'click').shareReplay(1);
+const scroll$     = Observable.fromEvent(document, 'mousewheel').shareReplay(1);
 const scrollUp$   = scroll$.filter((e) => e.wheelDelta >= 120);
 const scrollDown$ = scroll$.filter((e) => e.wheelDelta <= -120);
 
