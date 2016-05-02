62247fcfe2f777b51b3ed1ba0996c96d11a081c2
patrickmarabeas <patrick@marabeas.io>
2016-05-02 12:25:36 +1000

    Simplify implementation across tests

--- src/main.js
+++ src/main.js
@@ -10,45 +10,29 @@ import React    from 'react';
 import ReactDOM from 'react-dom';
 
 
-
-
 /**
  * 1 - Stateless + Higher Order Components
  *
- */
-import App from './1-Stateless+HigherOrderComponents/App.js';
-
-ReactDOM.render(<App />, document.getElementById('app1'));
-
-
-
-
-/**
  * 2 - RxJS + Stateless Components
- *
  * - Currently only produces singletons
  * - Needs method to set Observables on elements within the component
  *
- */
-import App2 from './2-RxJS+StatelessComponents/App.js';
-
-// Subscribe to the components MapObservable stream
-App2().subscribe(app => {
-  ReactDOM.render(app, document.getElementById('app2'));
-});
-
-
-
-
-/**
  * 3 - RxJS + Dispatcher + Stateless Components
- *
  * - Still only dealing with singletons
  * - Need to improve on manner App component constructs its contents
  *
+ * 4 - Parent Models and Intents
+ *
+ *
  */
-import App3 from './3-RxJS+Dispatcher+StatelessComponents/App.js';
 
-App3().subscribe(app => {
-  ReactDOM.render(app, document.getElementById('app3'));
+//import App from './1-Stateless+HigherOrderComponents/App.js';
+//ReactDOM.render(<App />, document.getElementById('app'));
+
+//import App from './2-RxJS+StatelessComponents/App.js';
+import App from './3-RxJS+Dispatcher+StatelessComponents/App.js';
+//import App from './4-ParentModels+Intents/App.js';
+
+App().subscribe(app => {
+  ReactDOM.render(app, document.getElementById('app'))
 });
